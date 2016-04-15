4bfb3396bad1b99497c559f5b017ded27be7d459
gunqor <gunqor0@gmail.com>
2016-04-15 10:35:13 +0300

    initial commit

---
+++ BeginningGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeginningGit.Startup))]
+namespace BeginningGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
